e480c6cdb2194b6b3393b896e57995096dbf4c53
Holger Grosse-Plankermann <holgergp@gmail.com>
2022-01-21 11:20:45 +0100

    Make linter happy

--- src/components/LeagueTable/LeagueTable.js
+++ src/components/LeagueTable/LeagueTable.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Position from '../Position/Position';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
